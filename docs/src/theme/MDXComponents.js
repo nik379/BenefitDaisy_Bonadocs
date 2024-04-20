2d101359f567c35c89b4c6deab5330e791b4402a
nik379 <nikhilgupta3501@gmail.com>
2024-04-20 16:44:21 +0530

    Create MDXComponents.js

---
+++ docs/src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import BonadocsWidget from '@bonadocs/widget';
+
+export default {
+  ...MDXComponents,
+  BonadocsWidget
+};
